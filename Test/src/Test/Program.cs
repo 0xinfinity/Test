8a2be29039ecda4256b01bbdd88aa85359a4c678
0xinfinity <dominiksalach@gmail.com>
2017-02-24 08:57:57 +0100

    change configuration

--- Test/src/Test/Program.cs
+++ Test/src/Test/Program.cs
@@ -16,6 +16,7 @@ namespace Test
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:80")
                 .Build();
 
             host.Run();
